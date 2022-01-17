0cbd58f14bf449fe2b704d337f2bcfafa2dff671
jbiers <juliabiersuriano@gmail.com>
2022-01-17 00:30:08 -0400

    created book object

---
+++ script.js
@@ -0,0 +1,16 @@
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function () {
+        if (read) {
+            return `${title} by ${author} , ${pages} pages, has been read`;
+        }
+
+        else {
+            return `${title} by ${author} , ${pages} pages, not read yet`;
+        }
+    };
+};
